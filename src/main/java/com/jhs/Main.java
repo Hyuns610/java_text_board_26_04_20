3e5fffd7cccb280332f0bc8a51135c3d7a62f93e
Hyuns610 <hunsik33@naver.com>
2026-04-21 00:00:27 +0900

    요구사항1 완료

--- src/main/java/com/jhs/Main.java
+++ src/main/java/com/jhs/Main.java
@@ -1,8 +1,29 @@
 package com.jhs;
 
+import java.util.Scanner;
+
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
   public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("== 자바 게시판 시작 ==");
+
+    while (true) {
+      System.out.print("명령) ");
+      String cmd = sc.nextLine();
+
+      if (cmd.equals("exit")) {
+        System.out.println("프로그램을 종료합니다.");
+        break;
+      }
+
+      System.out.println("입력받은 명령어 : " + cmd);
+    }
+
+    System.out.println("== 자바 게시판 종료 ==");
+
+    sc.close();
   }
 }
