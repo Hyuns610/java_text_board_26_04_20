552d594f38084c03a2774f7003c4c346569c69cb
Hyuns610 <hunsik33@naver.com>
2026-04-26 22:53:54 +0900

    게시물 테스트 데이터를 별도의 함수로 처리

--- src/main/java/com/jhs/Main.java
+++ src/main/java/com/jhs/Main.java
@@ -5,14 +5,24 @@ import com.jhs.domain.article.Article;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
+  static void makeArticleTestData(List<Article> articles) {
+    IntStream.rangeClosed(1, 5)
+        .forEach(
+            i -> articles.add(new Article(i, "제목" + i, "내용" + i))
+        );
+  }
+
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     List<Article> articles = new ArrayList<>();
     int lastId = 0;
+
+    makeArticleTestData(articles);
 
     System.out.println("== 자바 게시판 시작 ==");
 
