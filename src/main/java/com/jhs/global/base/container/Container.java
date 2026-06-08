46232503220c223d6cff1accfa1d07079a0a6e33
Hyuns610 <hunsik33@naver.com>
2026-06-08 13:52:58 +0900

    ArticleService 도입

--- src/main/java/com/jhs/global/base/container/Container.java
+++ src/main/java/com/jhs/global/base/container/Container.java
@@ -1,16 +1,22 @@
 package com.jhs.global.base.container;
 
 import com.jhs.domain.article.controller.ArticleController;
+import com.jhs.domain.article.service.ArticleService;
 
 import java.util.Scanner;
 
 public class Container {
   public static Scanner sc;
 
+  public static ArticleService articleService;
+
   public static ArticleController articleController;
 
   static {
     sc = new Scanner(System.in);
+
+    articleService = new ArticleService();
+
     articleController = new ArticleController();
   }
 }
