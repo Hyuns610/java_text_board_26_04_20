eefe13db94ba0a8b152f00498fd37219265a3379
Hyuns610 <hunsik33@naver.com>
2026-06-08 13:32:41 +0900

    컨트롤러도 컨테이너에 의해 생성되게 변경

--- src/main/java/com/jhs/global/base/container/Container.java
+++ src/main/java/com/jhs/global/base/container/Container.java
@@ -1,11 +1,16 @@
 package com.jhs.global.base.container;
 
+import com.jhs.domain.article.controller.ArticleController;
+
 import java.util.Scanner;
 
 public class Container {
   public static Scanner sc;
 
+  public static ArticleController articleController;
+
   static {
     sc = new Scanner(System.in);
+    articleController = new ArticleController();
   }
 }
