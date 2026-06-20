6426775fc9ae0057db9ba0331254e8c1762ea11f
Hyuns610 <hunsik33@naver.com>
2026-06-20 13:27:40 +0900

    url 쿼리스트링 분석을 Util 클래스를 통해 처리

--- src/test/java/QueryStringParsingTest.java
+++ src/test/java/QueryStringParsingTest.java
@@ -5,35 +5,43 @@ import java.util.Map;
 public class QueryStringParsingTest {
   public static void main(String[] args) {
     String url = "/usr/article/list?page=1&code=title&q=제목1 입니까? 여기는..&calc=[10+20=]";
+    Map<String, String> params1 = Ut.getParamFromUrl(url);
+    String urlPath = Ut.getPathFromUrl(url);
+    System.out.println("params1 : " + params1);
+    System.out.println("urlPath : " + urlPath);
 
-    Map<String, String> params =  new HashMap<>();
+    url="/usr/article/list";
+    Map<String, String> params2 = Ut.getParamFromUrl(url);
+    urlPath = Ut.getPathFromUrl(url);
+    System.out.println("params2 : " + params2);
+    System.out.println("urlPath : " + urlPath);
+  }
 
-    String[] urlBits = url.split("\\?", 2);
-    String queryString = urlBits[1];
-    // System.out.println(Arrays.toString(urlBits));
+  class Ut {
+    public static Map<String, String> getParamFromUrl(String url) {
+      Map<String, String> params =  new HashMap<>();
+      String[] urlBits = url.split("\\?", 2);
 
-    System.out.println(queryString);
+      if(urlBits.length == 1) return params;
 
-    String[] queryBits = queryString.split("&");
-    // [page=1, code=title, q=제목1 입니까? 여기는..]
+      String queryString = urlBits[1];
 
-    for(String queryBit : queryBits) {
-      String [] queryBitBits = queryBit.split("=", 2);
+      String[] queryBits = queryString.split("&");
+      for(String queryBit : queryBits) {
+        String [] queryBitBits = queryBit.split("=", 2);
 
-      /*
-      System.out.println(queryBitBits[0]);
-      System.out.println(queryBitBits[1]);
-      */
-      String paramName = queryBitBits[0];
-      String paramValue = queryBitBits[1];
+        String paramName = queryBitBits[0];
+        String paramValue = queryBitBits[1];
 
-      params.put(paramName, paramValue);
+        params.put(paramName, paramValue);
+      }
+
+      return params;
     }
 
-    // System.out.println(params);
-    System.out.println("== 원하는 쿼리 데이터 가져오기 ==");
-    System.out.printf("page : %d\n", Integer.parseInt(params.get("page")));
-    System.out.printf("code : %s\n", params.get("code"));
-    System.out.printf("q : %s\n", params.get("q"));
+    public static String getPathFromUrl(String url) {
+      String[] urlBits = url.split("\\?", 2);
+      return urlBits[0];
+    }
   }
 }
